c333fe113a03c54df7de278c4a33affea8621995
Avalanche7CZ <avalanche752@gmail.com>
2025-06-27 14:38:38 +0200

    Enhance DebugLogger with dynamic log level control
    Added dynamic log level configuration using Log4j2's Configurator based on the debugEnable flag in the main config. Updated log messages to use appropriate log levels (debug/warn) and improved logger naming for clarity. Ensured status message about debug logging is only logged once.

--- src/main/java/eu/avalanche7/paradigm/utils/DebugLogger.java
+++ src/main/java/eu/avalanche7/paradigm/utils/DebugLogger.java
@@ -1,32 +1,46 @@
 package eu.avalanche7.paradigm.utils;
 
 import eu.avalanche7.paradigm.configs.MainConfigHandler;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class DebugLogger {
-    private static final Logger SLF4J_LOGGER = LoggerFactory.getLogger("Paradigm");
+    private static final Logger SLF4J_LOGGER = LoggerFactory.getLogger("eu.avalanche7.paradigm");
     private final MainConfigHandler.Config mainConfig;
+    private static final AtomicBoolean hasLoggedStatus = new AtomicBoolean(false);
 
     public DebugLogger(MainConfigHandler.Config mainConfig) {
         this.mainConfig = mainConfig;
+        if (!hasLoggedStatus.getAndSet(true)) {
+            boolean debugEnabledInConfig = mainConfig != null && mainConfig.debugEnable;
+
+            if (debugEnabledInConfig) {
+                Configurator.setLevel("eu.avalanche7.paradigm", Level.DEBUG);
+                SLF4J_LOGGER.info("[Paradigm] Debug logging is ENABLED. Verbose logs will now be shown.");
+            } else {
+                Configurator.setLevel("eu.avalanche7.paradigm", Level.INFO);
+                SLF4J_LOGGER.info("[Paradigm] Debug logging is DISABLED. To see verbose logs, enable 'debugEnable' in the main config.");
+            }
+        }
     }
 
     public void debugLog(String message) {
         if (mainConfig != null && mainConfig.debugEnable) {
-            SLF4J_LOGGER.info(message);
+            SLF4J_LOGGER.debug("[Paradigm-Debug] " + message);
         }
     }
-
     public void debugLog(String message, Exception e) {
         if (mainConfig != null && mainConfig.debugEnable) {
-            SLF4J_LOGGER.error(message, e);
+            SLF4J_LOGGER.warn("[Paradigm-Debug] " + message, e);
         }
     }
-
     public void debugLog(String message, Object... args) {
         if (mainConfig != null && mainConfig.debugEnable) {
-            SLF4J_LOGGER.info(message, args);
+            SLF4J_LOGGER.debug("[Paradigm-Debug] " + message, args);
         }
     }
 }
